3b41120aeebc3462a3c8d42ff28e23d4364e5490
Santiago  Pedroza <65375294+snat-s@users.noreply.github.com>
2022-05-04 19:31:13 +0000

    Added a bunch of features and a license, modified a bit the example

--- index.js
+++ index.js
@@ -1,121 +1,251 @@
 fs = require('fs')
-// ADDME
-/* const GlobalVariables = [
-	title: '#+title:',
-	author: '#+author:',
-	date: '#+date:',
-	begin_src: '#+begin_src',
-	end_src: '#+end_src'
-] */
+const args = process.argv.slice(2);
+//console.log(args);
+const file = args[0] || './sample';
+
+function parseNormalLine(line) {
+
+	let parsedLine = "";
+	for (let i = 0; i < line.length; i++) {
+		switch (line.charAt(i)) {
+			case '*': {
+				i++;
+				parsedLine += "\<b\>";
+				//console.log(parsedLine);
+				while (line[i] != '*') {
+					parsedLine += line[i]
+					i++;
+				}
+
+				parsedLine += "\<\/b\> ";
+				//console.log(parsedLine);
+			} break;
+			case '/': {
+				i++;
+				parsedLine += "\<em\>";
+				while (line[i] != '/') {
+					parsedLine += line[i]
+					i++;
+				}
+				i++;
+				parsedLine += "\<\/em\> "
+			} break;
+			case '=': {
+				i++;
+				parsedLine += "\<code\>";
+				while (line[i] != '=') {
+					parsedLine += line[i]
+					i++;
+				}
+				i++;
+				parsedLine += "\<\/code\> "
+			} break;
+			case '\[': {
+				if (line[i + 1] == '[') {
+					let currentParsing = "";
+					for (let j = i + 2; j < line.length - 1; j++) {
+						//console.log(j);
+						 if (line[j] == ']' && line[j + 1] != ']') {
+							// we have a hyperlink
+							let k = j + 1;
+							while (line[k] != '[' && k < line.length) k++;
+							k++;
+							let description = "";
+							while (line[k] != ']' && k < line.length) {
+								description += line[k];
+								k++;
+							}
+							 k++;
+							i = j = k;
+							
+							parsedLine = parsedLine + '\<a href=\"' + currentParsing + '\"> ' + description + '<\/a>';
+							 break;
+						}
+						currentParsing += line[j];
+					}
+				}
+			} break;
+			default: {
+				parsedLine += line[i];
+			}
+		}
+		//console.log(line[i]);
+	}
+	//console.log(`Esta es la línea parseada: \n${parsedLine}\nLínea normal:\n ${line}`);
+	return parsedLine;
+}
+
+function parseForLinks(line) {
+	let parsedLine = "";
+	//console.log(line);
+	for (let i = 0; i < line.length - 1; i++) {
+
+		if (line[i] == '[' && line[i + 1] == '[') {
+
+			for (let j = i + 2; j < line.length - 1; j++) {
+				if (line[j] == ']' && line[j + 1] == ']') {
+					// we have a picture
+					return '\<img src=\"' + parsedLine + '\" alt=\"' + parsedLine + '\"\>';
+				} else if (line[j] == ']' && line[j + 1] != ']') {
+					// we have a hyperlink
+
+					let k = j + 1;
+					while (line[k] != '[')
+						k++;
+					k++;
+					let description = "";
+					while (line[k] != ']') {
+						description += line[k];
+						k++;
+					}
+					return '\<a href=\"' + parsedLine + '\"> ' + description + '<\/a>';
+				}
+				parsedLine += line[j];
+				i = j;
+			}
+
+		}
+	}
+	return line;
+}
 
 try {
-	const data = fs.readFileSync('./sample.org', 'utf8');
+	const data = fs.readFileSync(file + '.org', 'utf8');
 	const lines = data.split(/\r?\n/);
-	let finalResult = "\<body\>\n";
-	let headerMetadata = "\<header\>"
-		+ "\n\<meta charset=\"utf-8\"\>\n" 
-		+ "\<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\\/\>" 
+	let body = "\<body\>\n \<div id=\"content\"\>\n";
+	let headerMetadata = "\<head\>"
+		+ "\n\<meta charset=\"utf-8\"\>\n"
+		+ "\<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"\/\>"
 		+ "\n\<link rel=\"stylesheet\"href=\"https://cdn.simplecss.org/simple.min.css\">\n";
 	let footer = "\<footer\>\n";
 	// adding content
-	for(let i = 0; i < lines.length; i++) {
+	for (let i = 0; i < lines.length; i++) {
 		// titles and subtitles
-		if(lines[i] === '' || lines[i] === '\n') {
+		//console.log(i);
+		if (lines[i] === '' || lines[i] === '\n') {
 			continue;
 		}
-		if(lines[i][0] == '*') {
+		if (lines[i][0] == '*') {
 			let headerNumber = 0;
-			while(lines[i][headerNumber] === '*') {
-				headerNumber++; 
+			while (lines[i][headerNumber] === '*') {
+				headerNumber++;
 			}
+			//FIXME: finish this case for a normal line.
+			/*if(lines[i][headerNumber+1] !== ' ') {
+				body += parseNormalLine(lines[i]);
+				continue;
+			}*/
 			let title = "";
-			for(let j = headerNumber+1; j < lines[i].length; j++) {
-				title+=lines[i][j];
+			for (let j = headerNumber + 1; j < lines[i].length; j++) {
+				title += lines[i][j];
 			}
-			finalResult += '\<h' + headerNumber +'\>' + title +'\<\/h' + headerNumber + '\>\n';
-		} 
+			body += '\<h' + headerNumber + '\>' + title + '\<\/h' + headerNumber + '\>\n';
+		}
 		// bullet lists
 		// TODO: embedded lists
-		else if(lines[i][0] === '-' || lines[i][0] === '+') {
-			finalResult += "\n\<ul\>\n";
-			while(lines[i][0] === '-' || lines[i][0] === '+') {
-				finalResult += "\<li\>";
+		else if (lines[i][0] === '-' || lines[i][0] === '+') {
+			body += "\n\<ul\>\n";
+			while (lines[i][0] === '-' || lines[i][0] === '+' && i < lines.length) {
+				body += "\<li\>";
 				let line = "";
-				for(let j = 2; j < lines[i].length; j++) {
-					line+=lines[i][j];
+				for (let j = 2; j < lines[i].length; j++) {
+					line += lines[i][j];
 				}
-				finalResult += line;
-				finalResult += "\<\/li\>\n";
+				body += line;
+				body += "\<\/li\>\n";
 				i++;
 			}
-			finalResult += "\<\/ul\>\n";
+			body += "\<\/ul\>\n";
 		}
-		// TODO: numbered bullet lists
-		else if(lines[i][0] >= '0' && lines[i][0] <= '9') {
-			
-			finalResult += "\n\<ul\>\n";
-			while(lines[i][0] >= '0' && lines[i][0] <= '9' && i < lines.legth) {
+		// DONE: numbered bullet lists
+		else if (lines[i][0] >= '0' && lines[i][0] <= '9') {
+			let j = 0;
+			while (lines[i][j] >= '0' && lines[i][j] <= '9') j++;
+			if (lines[i][j] != '.') {
+				//console.log(lines[i][j]);
+				body += '\n<p>'+ parseNormalLine(lines[i]) + '<\/p>\n';
+				continue;
+			}
+			body += "\n\<ol\>\n";
+			//console.log("Mi mama me quiere");
+			while (lines[i][0] >= '0' && lines[i][0] <= '9' && i < lines.length) {
 				let j = 0;
-				console.log(`Estoy aquí`,i);
-				let currentLine = "\<ol\>";
-				while(lines[i][j] >= '0' && lines[i][j] <= '9') j++;
-				if(lines[i][j] !== '.') {
-					console.log(lines[i][j]);
-					break;
+				//console.log('HOLA' + i);
+				let currentLine = "\<li\>";
+				while (lines[i][j] >= '0' && lines[i][j] <= '9') j++;
+				if (lines[i][j] != '.') {
+					//console.log(lines[i][j]);
+					body += '\n<p>'+ parseNormalLine(lines[i])+'<\/p>\n';
+					continue;
 				}
 				j++;
-				for(; j < lines[i].length; j++) {
+				for (; j < lines[i].length; j++) {
 					currentLine += lines[i][j];
 				}
-				currentLine += "\<\/ol\>\n";
-				console.log(currentLine);
-				finalResult += currentLine;
+				currentLine += "\<\/li\>\n";
+				//console.log(currentLine);
+				body += currentLine;
 				i++;
 			}
-			finalResult += "\<\/ul\>\n"
+			body += "\<\/ol\>\n"
 		}
 		// header or comment
-		else if(lines[i][0] === '#') {
-			if(lines[i][1] === ' ') {
+		else if (lines[i][0] === '#') {
+			if (lines[i][1] === ' ') {
 				// this is a comment
 				continue;
 			} else {
 				let parsedValue = "", j = 0, content = "";
-				while(lines[i][j] !== ':') {
+				while (lines[i][j] !== ':') {
 					parsedValue += lines[i][j];
 					j++;
 				}
 				j++;
-				for(; j < lines[i].length; j++) {
+				for (; j < lines[i].length; j++) {
 					content += lines[i][j];
 				}
-				if(parsedValue === "#+title") {
+				if (parsedValue === "#+title") {
 					//console.log("soy un titulo");
-					headerMetadata += "\<title\>"+ content +"\<\/title\>\n"				
+					headerMetadata += "\<title\>" + content + "\<\/title\>\n"
 				}
-				else if(parsedValue === "#+author") {
+				else if (parsedValue === "#+author") {
 					footer += content;
 					headerMetadata += "\<meta name=\"author\" content=\"" + content + " \"\>"
 					continue;
 				}
-				else if(parsedValue === "#+date") {
+				else if (parsedValue === "#+date") {
 					footer += content;
 				}
 			}
 		}
-		// Texto normal
-			// 	FIXME: que las líneas no sean solo de una sola frase larga y puedan ser múltiples líneas.
-		else {
-			finalResult += '\n\<\/p\>' + lines[i] +  "\<\/p\>\n";
+		// tables
+		else if (lines[i][0] == '|') {
+			/*body += "\<table\>";
+			
+			while() {
+				
+			}
+			body += "\<\/table\>";*/
 		}
-		if(i > lines.length) break;
+		// images
+		else if (lines[i][0] == '\[') {
+			body += '\n' + parseForLinks(lines[i]) + '\n';
+		}
+		// Texto normal
+		// 	FIXME: que las líneas no sean solo de una sola frase larga y puedan ser múltiples líneas.
+		else {
+
+			//currentLine = parseForLinks(lines[i]);
+			body += '\n\<\/p\>' + parseNormalLine(lines[i]) + "\<\/p\>\n";
+		}
+		//if (i > lines.length) break;
 	}
 	footer += "\n\<\/footer\>";
 	headerMetadata += "\n\<\/head\>\n\n";
-	finalResult += "\n\<\/body\>\n\n";
-	finalResult = headerMetadata + finalResult + footer;
-	console.log(finalResult);
-	fs.writeFileSync('sample.html', finalResult);
+	body += "\n\<\/div>\n\<\/body\>\n\n";
+	body = headerMetadata + body + footer;
+	//console.log(body);
+	fs.writeFileSync(file + '.html', body);
 } catch (err) {
 	console.log(err);
 }
